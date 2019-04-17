4f41fb425a276c5418fd1488dda92e3a4b491bc0
Obpumanee <obpumanee@gmail.com>
2019-04-17 14:55:34 +1000

    added controler and metods

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplcationController 
+    def home
+        render plain: "homepage"
+    end 
+end
--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,4 @@
 class Breed < ApplicationRecord
+    has_many :breed  
+    enum sex: { female: 0, male: 1 }
 end
